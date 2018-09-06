7a6a33056d6598d1eb2d7202795eab1b74e51245
Sam Anklesaria <ankle@mit.edu>
2018-09-06 14:55:04 -0400

    simplified for single value plotting

--- src/VisdomLog.jl
+++ src/VisdomLog.jl
@@ -1,38 +1,36 @@
 module VisdomLog
-export Visdom, report
+export Visdom, report, inform
 using PyCall
 @pyimport visdom
 @pyimport numpy as np
 
 struct Visdom
   vis::PyObject
-  d::Dict{Symbol,Union{Nothing, Pair{PyObject,Array{Int}}}}
+  d::Dict{Symbol,Union{Nothing, Pair{PyObject,Any}}}
 end
 
-Visdom(env::String, ks::Array{Symbol}) =
-  Visdom(visdom.Visdom(env=env),
-    Dict(k=>Nullable() for k in ks))
+function Visdom(env::String, ks::Array{Symbol})
+  vd = visdom.Visdom(env=env)
+  Visdom(vd, Dict(k=>nothing for k in ks))
+end
     
-function report(l, k::Symbol, vals...)
-  y = hcat(vals...)'
+function report(l, k::Symbol, y)
   if l.d[k] == nothing
-    x = (0:(length(vals) - 1)) .+ fill(1, length(vals[1]))'
-    y = hcat(vals...)'
-    win = l.vis[:line](X=x, Y=y, opts=Dict(:title=>String(k)))
-    l.d[k] = (win => x + length(x))
+    win = l.vis[:line](X=[1], Y=[y], opts=Dict(:title=>String(k)))
+    l.d[k] = (win => 2)
   else
-    (win, x) = get(l.d[k])
-    l.vis[:line](win=win, X=x, Y=y, update="append", opts=Dict("ytype"=>"log"))
-    l.d[k] = (win => x + size(x)[2])
+    (win, x) = l.d[k]
+    l.vis[:line](win=win, X=[x], Y=[y], update="append", opts=Dict("ytype"=>"log"))
+    l.d[k] = (win => x + 1)
   end
 end
 
 function inform(l, k::Symbol, vals)
   if l.d[k] == nothing
     win = l.vis[:histogram](X=vals[:], opts=Dict(:title=>String(k)))
-    l.d[k] = (win=>Int[])
+    l.d[k] = win
   else
-    (win, x) = get(l.d[k])
+    (win, x) = l.d[k]
     l.vis[:histogram](win=win, X=vals[:], opts=Dict("ytype"=>"log"))
   end
 end
