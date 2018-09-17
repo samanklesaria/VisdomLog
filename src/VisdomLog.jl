a97a3a8df3a557d8803efd98b0e9c90d66cd77f3
Sam Anklesaria <ankle@mit.edu>
2018-09-17 16:53:43 -0400

    log labels are dynamic

--- src/VisdomLog.jl
+++ src/VisdomLog.jl
@@ -6,32 +6,32 @@ using PyCall
 
 struct Visdom
   vis::PyObject
-  d::Dict{Symbol,Union{Nothing, Pair{PyObject,Any}}}
+  d::Dict{Symbol, Pair{PyObject,Any}}
 end
 
-function Visdom(env::String, ks::Array{Symbol})
+function Visdom(env::String)
   vd = visdom.Visdom(env=env)
-  Visdom(vd, Dict(k=>nothing for k in ks))
+  Visdom(vd, Dict{Symbol, Pair{PyObject,Any}}())
 end
     
 function report(l, k::Symbol, y)
-  if l.d[k] == nothing
+  if !haskey(l.d, k)
     win = l.vis[:line](X=[1], Y=[y], opts=Dict(:title=>String(k)))
-    l.d[k] = (win => 2)
+    l.d[k] = (win => 2);
   else
     (win, x) = l.d[k]
     l.vis[:line](win=win, X=[x], Y=[y], update="append", opts=Dict("ytype"=>"log"))
-    l.d[k] = (win => x + 1)
+    l.d[k] = (win => x + 1);
   end
 end
 
 function inform(l, k::Symbol, vals)
-  if l.d[k] == nothing
+  if !haskey(l.d, k)
     win = l.vis[:histogram](X=vals[:], opts=Dict(:title=>String(k)))
-    l.d[k] = win
+    l.d[k] = win=>nothing;
   else
-    (win, x) = l.d[k]
-    l.vis[:histogram](win=win, X=vals[:], opts=Dict("ytype"=>"log"))
+    (win, _) = l.d[k]
+    l.vis[:histogram](win=win, X=vals[:], opts=Dict("ytype"=>"log"));
   end
 end
 end
