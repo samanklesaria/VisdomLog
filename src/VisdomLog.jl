9d958e37ce3ef5ae55aca668a2312caf1e997e7d
Sam Anklesaria <ankle@mit.edu>
2018-09-25 10:36:45 -0400

    added matplotlib support

--- src/VisdomLog.jl
+++ src/VisdomLog.jl
@@ -1,6 +1,6 @@
 __precompile__(false) 
 module VisdomLog
-export Visdom, report
+export Visdom, report, matplot
 using PyCall
 @pyimport visdom
 @pyimport numpy as np
@@ -21,7 +21,7 @@ function report(l, k::Symbol, y::Float64)
     l.d[k] = (win => 2);
   else
     (win, x) = l.d[k]
-    l.vis[:line](win=win, X=[x], Y=[y], update="append", opts=Dict("ytype"=>"log"))
+    l.vis[:line](win=win, X=[x], Y=[y], update="append", opts=Dict(:ytype=>"log"))
     l.d[k] = (win => x + 1);
   end
 end
@@ -32,8 +32,18 @@ function report(l, k::Symbol, vals::Array{Float64})
     l.d[k] = win=>nothing;
   else
     (win, _) = l.d[k]
-    l.vis[:histogram](win=win, X=vals[:], opts=Dict("ytype"=>"log", :title=>String(k)));
+    l.vis[:histogram](win=win, X=vals[:], opts=Dict(:ytype=>"log", :title=>String(k)));
   end
 end
+
+function matplot(l, k::Symbol, fig)
+  if !haskey(l.d, k)
+    win = l.vis[:matplot](plot=fig, opts=Dict(:title=>String(k)))
+    l.d[k] = win=>nothing;
+  else
+    (win, _) = l.d[k]
+    l.vis[:matplot](win=win, plot=fig, opts=Dict(:title=>String(k)));
+  end
 end
 
+end
