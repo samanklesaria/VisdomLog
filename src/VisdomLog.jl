e443d24f4f11caddfbc6aa7f5530de7c4b69447c
Sam Anklesaria <anklesaria@utexs.edu>
2023-01-18 09:26:32 -0600

    simplify interface

--- src/VisdomLog.jl
+++ src/VisdomLog.jl
@@ -1,78 +1,56 @@
 module VisdomLog
-export Visdom, report
+export Visdom, report, scatter, save
 using PyCall
-using PyPlot: Figure
 
 struct Visdom
   vis::PyObject
-  d::Dict{Symbol, Pair{PyObject,Any}}
 end
 
 "Start a new visdom environment"
 function Visdom(env::String)
   visdom = pyimport(:visdom)
   vd = visdom[:Visdom](env=env)
-  Visdom(vd, Dict{Symbol, Pair{PyObject,Any}}())
+  Visdom(vd)
 end
 
-# ABSTRACT this pattern!
+pathstr(a::String) = a
+pathstr(a::Symbol) = String(a)
+pathstr(a::Tuple) = join(pathstr.(a), ".")
+
+"Save state to disk"
+function save(vd::Visdom, envs)
+  vd.vis[:save](envs)
+end
 
 "Add a value to a log of results"
-function report(l, k::Symbol, y::Float64)
-  if !haskey(l.d, k)
-    win = l.vis[:line](X=[1], Y=[y], opts=Dict(:title=>String(k)))
-    l.d[k] = (win => 2);
-  else
-    (win, x) = l.d[k]
-    l.vis[:line](win=win, X=[x], Y=[y], update="append", opts=Dict(:ytype=>"log"))
-    l.d[k] = (win => x + 1);
-  end
+function report(l, k, x::Number, y::Number; log=true)
+  ytype = log ? "log" : "linear"
+  kstr = pathstr(k)
+  l.vis[:line](win=kstr, X=[x], Y=[y], update="append",
+    opts=Dict(:ytype=>ytype, :title=>kstr))
 end
 
 "Display a histogram of current results"
-function report(l, k::Symbol, vals::Array{Float64})
-  if !haskey(l.d, k)
-    win = l.vis[:histogram](X=vals[:], opts=Dict(:title=>String(k)))
-    l.d[k] = win=>nothing;
-  else
-    (win, _) = l.d[k]
-    l.vis[:histogram](win=win, X=vals[:], opts=Dict(:ytype=>"log", :title=>String(k)));
-  end
+function report(l, k, vals::AbstractArray{<: Number})
+  kstr = pathstr(k)
+  l.vis[:histogram](win=kstr, X=vals[:], opts=Dict(:ytype=>"log", :title=>kstr));
+end
+
+function scatter(l::Visdom, k, xs)
+  kstr = pathstr(k)
+  l.vis[:scatter](win=kstr, X=xs, opts=Dict(:title=>kstr));
 end
 
 "Plot a comparison of current results"
-function report(l, k::Symbol, (xs, ys, legend)::Tuple{Vector,Matrix{Float64},Vector})
-  if !haskey(l.d, k)
-    win = l.vis[:line](X=xs, Y=ys, opts=Dict(:legend=>legend, :title=>String(k)))
-    l.d[k] = win=>nothing;
-  else
-    (win, _) = l.d[k]
-    l.vis[:line](win=win, X=xs, Y=ys, update="replace",
-      opts=Dict(:legend=>legend, :title=>String(k)));
-  end
+function report(l, k, (xs, ys, legend)::Tuple{AbstractVector,AbstractMatrix{<: Number},AbstractVector})
+  kstr = pathstr(k)
+  l.vis[:line](win=kstr, X=xs, Y=ys, opts=Dict(:legend=>legend, :title=>kstr))
 end
 
 "Plot a comparison of current results"
-function report(l, k::Symbol, (xs, ys)::Tuple{Vector,Vector{Float64}})
-  if !haskey(l.d, k)
-    win = l.vis[:line](X=xs, Y=ys, opts=Dict(:title=>String(k)))
-    l.d[k] = win=>nothing;
-  else
-    (win, _) = l.d[k]
-    l.vis[:line](win=win, X=xs, Y=ys, update="replace",
-      opts=Dict(:title=>String(k)));
-  end
-end
-
-"Plot a matplotlib figure"
-function report(l, k::Symbol, fig::Figure)
-  if !haskey(l.d, k)
-    win = l.vis[:matplot](plot=fig, opts=Dict(:title=>String(k)))
-    l.d[k] = win=>nothing;
-  else
-    (win, _) = l.d[k]
-    l.vis[:matplot](win=win, plot=fig, opts=Dict(:title=>String(k)));
-  end
+function report(l, k, (xs, ys)::Tuple{AbstractArray,AbstractArray{<: Number}})
+  kstr = pathstr(k)
+  l.vis[:line](win=kstr, X=xs, Y=ys, opts=Dict(:title=>kstr))
 end
 
 end
